3449283514e6b53f6f3b56d79595ffd4c7812519
dggrom <dg-grom@mail.ru>
2019-02-21 02:03:10 +0300

    Last redact 2019/02/21

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,32 @@
-package sample;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private AnchorPane Fome;
+
+    @FXML
+    private Button FormButtonConnect;
+
+    @FXML
+    private TextField FormEditLogin;
+
+    @FXML
+    private TextField FormEditPass;
+
+    @FXML
+    void initialize() {
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Подключение к 1С");
+        primaryStage.setScene(new Scene(root, 840, 363));
         primaryStage.show();
     }
 
